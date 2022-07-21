d68fa42603eaa3bca3566687e9f9a8c5e1e0124d
sahandbabali <sahand.babali@gmail.com>
2022-07-22 02:21:39 +0430

    main color removed

--- app.js
+++ app.js
@@ -28,43 +28,7 @@ function extractcolors() {
 
 function createpaletteui() {
   const img = document.getElementById("results-image");
-  console.log(colorThief.getColor(img));
-  let domcolor = colorThief.getColor(img);
-  let hexvalue = rgbToHex(domcolor[0], domcolor[1], domcolor[2]);
 
-  let domcolorr = Math.round((domcolor[0] * 100) / 256);
-  let domcolorg = Math.round((domcolor[1] * 100) / 256);
-  let domcolorb = Math.round((domcolor[2] * 100) / 256);
-
-  document.getElementById("dominantcolordiv").innerHTML = `<div class="row">
-    <div style="background-color: ${hexvalue}; color: white" class="col-12 pt-2 pb-2">
-        ${hexvalue}
-
-        <div class="row">
-        <div class="col-4  ">
-              <p>R</p>
-              <div class="progress">
-  <div class="progress-bar bg-danger " role="progressbar" aria-label="Basic example" style="width: ${domcolorr}%" aria-valuenow="${domcolorr}" aria-valuemin="0" aria-valuemax="100"></div>
-</div>
-
-        </div>
-        <div class="col-4 ">
-              <p>G</p>
-              <div class="progress">
-              <div class="progress-bar  bg-success" role="progressbar" aria-label="Basic example" style="width: ${domcolorg}%" aria-valuenow="${domcolorg}" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-        </div>
-        <div class="col-4">
-              <p>B</p>
-              <div class="progress">
-  <div class="progress-bar" role="progressbar" aria-label="Basic example" style="width: ${domcolorb}%" aria-valuenow="${domcolorb}" aria-valuemin="0" aria-valuemax="100"></div>
-</div>
-        </div>
-  </div>
-
-
-    </div>
-  </div>`;
   console.log(colorThief.getPalette(img, 6, 10));
   let palcolor = colorThief.getPalette(img, 6, 10);
   let paletterow = document.getElementById("paletterow");
